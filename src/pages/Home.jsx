b03936cc653adb4238bf7f22c7df291d620bd614
viniciuscanutx <viniciuscanutmz@gmail.com>
2024-08-17 12:11:27 -0300

    Finalizando estilização do MovieGrid

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -24,7 +24,6 @@ const Home = () => {
 
   return (
     <div className="container">
-      <h2 className="title">Melhores Filmes:</h2>
       <div className="movies-container">
         {topMovies.length === 0 && <p>Carregando... </p>}
         {topMovies.length > 0 && 
