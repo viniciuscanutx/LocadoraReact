501b0fb3c0e8e4e9b3e86b46468f446a36e0455e
viniciuscanutx <viniciuscanutmz@gmail.com>
2024-08-19 14:51:37 -0300

    Terminando a pagina de Movie e começando a estilização

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,7 +1,69 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import {
+  BsGraphUp,
+  BsHourglassSplit,
+  BsFillFileEarmarkTextFill
+} from "react-icons/bs"
+
+import './Movie.css'
+import MovieCard from "../components/MovieCard/MovieCard";
+
+const moviesURL = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
+
+
 const Movie = () => {
-    return (
-      <div>Movie</div>
-    )
+  const { id } = useParams()
+  const [movie, setMovie] = useState(null)
+
+  const getMovie = async (url) => {
+    const res = await fetch(url);
+    const data = await res.json();
+
+    setMovie(data);
   }
-  
-  export default Movie
+
+  const formatCurrency = (number) => {
+    return number.toLocaleString("en-US", {
+      style: "currency",
+      currency: "USD"
+    });
+  };
+
+  useEffect(() => {
+    const movieUrl = `${moviesURL}${id}?${apiKey}`;
+    getMovie(movieUrl);
+  }, [])
+
+  return (
+    <div>
+      {movie && (
+        <>
+          <MovieCard movie={movie} showLink={false} />
+          <p className="tagline">{movie.tagline}</p>
+          <div className="info description">
+            <h3>
+              <BsFillFileEarmarkTextFill /> Descrição:
+            </h3>
+            <p>{movie.overview}</p>
+          </div>
+          <div className="info">
+            <h3>
+              <BsHourglassSplit /> Duração:
+            </h3>
+            <p>{movie.runtime} minutos</p>
+          </div>
+          <div className="info">
+            <h3>
+              <BsGraphUp /> Receita:
+            </h3>
+            <p>{formatCurrency(movie.revenue)}</p>
+          </div>
+        </>
+      )}
+    </div>
+  )
+};
+
+export default Movie
